6ce94b23cda1f8603dfec23d2eaa0eed0976ade9
Dawid Dyrcz <dyrcu15@gmail.com>
2016-09-15 20:21:26 +0200

    Prepare auth logic

--- routers/auth.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router();
-
-router.post('login', (req, res) => {
-    // login to the application
-    // return token
-    res.end();
-});
-
-module.exports = router; 
