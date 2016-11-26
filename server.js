7a17628e2d236ce1c061987b8675db8f59ba2df7
Dawid Dyrcz <dyrcu15@gmail.com>
2016-11-26 16:53:11 +0100

    Handle application errors

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = require('express')(),
     mongoose = require('mongoose'),
     config = require('config'),
     cupboards = require('./lib/routers/cupboard'),
-    morgan = require('./lib/common/morgan_resolver');
+    morgan = require('./lib/common/morgan_resolver'),
+    response = require('./lib/common/responses');
 
 var port = process.env.PORT || '8000'
 
@@ -16,6 +17,12 @@ app.use(morgan());
 app.use('/', auth);
 app.use('/api/cupboards', cupboards);
 
-app.listen(port);
+app.use((err, req, res, next) => {
+    response.serverError(err, res);
+})
+
+app.listen(port, () => {
+    console.log('Magic happen on the port: ' + port);
+});
 
 module.exports = app;
